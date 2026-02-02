96bc52ab611256e5fafabcd6953d3921a75df424
Kintan Patel <kintanpatel4393@gmail.com>
2026-02-02 14:41:08 +0530

    Initial Commit

---
+++ ScreenProtection/ScreenProtectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenProtectionApp.swift
+//  ScreenProtection
+//
+//  Created by AK on 02/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ScreenProtectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
